eef27e50898fb34ec2233559b077927922cf9395
unknown <ishanmehta3636@gmail.com>
2026-09-11 16:59:23 -0700

    Deploy to remote

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "metroc-terraform-2025-tfstate"
+    bucket = "pragra-tfstate11092026"
     key    = "env/dev/terraform.tfstate"
     region = "ca-central-1"
   }
